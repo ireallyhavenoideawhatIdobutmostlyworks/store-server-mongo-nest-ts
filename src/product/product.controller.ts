763b14325270cc21910eb43ff0b3b318a1ef6033
m.urbaniak <urbaniak.michal@yahoo.com>
2022-09-04 08:19:35 +0200

    basic_controler_crud

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,9 +1,39 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Delete, Get, HttpCode, Param, Patch, Post, Res } from "@nestjs/common";
+import { ProductDto } from "./dto/product.dto";
 import { ProductService } from "./product.service";
 
 @Controller('api/product')
 export class ProductController {
 
-    public constructor(private readonly userService: ProductService) {}
+    public constructor(private readonly productService: ProductService) {}
 
+
+    @Get()
+    @HttpCode(200)
+    public findAll() {
+        return this.productService.findAll();
+    }
+
+    @Get(':uuid')
+    @HttpCode(200)
+    public findOneByUuid(@Param('uuid') uuid: string) {
+        return this.productService.findOneByUuid(uuid);
+    }
+
+    @Post()
+    public add(@Body() productDto: ProductDto) {
+        this.productService.add(productDto);
+    }
+
+    @Patch(':uuid')
+    @HttpCode(200)
+    public updateByUuid(@Param('uuid') uuid: string, @Body() productDto: ProductDto) {
+      return this.productService.updateByUuid(uuid, productDto);
+    }
+  
+    @Delete(':uuid')
+    @HttpCode(204)
+    public removeByUuid(@Param('uuid') uuid: string) {
+      return this.productService.removeByUuid(uuid);
+    }
 }
